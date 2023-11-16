d2af38670e7dbf90ba00e50394a369df472480ed
Alfredy87 <151002328+Alfredy87@users.noreply.github.com>
2023-11-16 16:10:05 +0300

    Add files via upload

---
+++ soud.js
@@ -0,0 +1,9 @@
+function show(){
+    var hello = document.getElementById("information");
+    if(hello.style.display === "none"){
+        hello.style.display = "block";
+    }
+    else{
+        hello.style.display = "none";
+    }
+}
